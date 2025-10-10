4398a3df3c5dc24d7d02cc6248300f6a696cb4e0
jdmv2605 <tu_correo@ejemplo.com>
2025-10-10 00:10:19 -0500

    agrego nuevo codigo

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,4 +1,7 @@
 import { Lusitana } from 'next/font/google';
+import { Inter } from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin'] });
 
 // Carga la fuente "Lusitana" de Google Fonts
 export const lusitana = Lusitana({
