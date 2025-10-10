109399eec9fcf4b79808ab83b791289c0058f0e3
jdmv2605 <tu_correo@ejemplo.com>
2025-10-09 21:16:57 -0500

    Corrijo archivo fonts.ts

---
+++ app/ui/fonts.ts
@@ -0,0 +1,7 @@
+import { Lusitana } from 'next/font/google';
+
+// Carga la fuente "Lusitana" de Google Fonts
+export const lusitana = Lusitana({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+});
